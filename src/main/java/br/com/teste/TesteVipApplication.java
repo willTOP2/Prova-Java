a12a529fdd11f86669479bdbff36401b09cc52b9
willTOP2 <willian.rodrygues146@gmail.com>
2021-11-10 13:17:46 -0300

    feature

---
+++ src/main/java/br/com/teste/TesteVipApplication.java
@@ -0,0 +1,13 @@
+package br.com.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteVipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteVipApplication.class, args);
+	}
+
+}
